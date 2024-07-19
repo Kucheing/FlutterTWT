6a5d181d62fc76db873b05b7737debcbdf3375cd
Faris Zulkiffli <22FTT1517@student.pb.edu.bn>
2024-07-19 20:19:18 +0800

    done with part 4 basically you entered messages and after sending it the message that you entered. It will then be display below the line and the message above will be cleared

--- lib/firstflutter.dart
+++ lib/firstflutter.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Faris App',
       theme: ThemeData(
-        primarySwatch: Colors.amber,
+        primarySwatch: Colors.purple,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ), //themeData
       home: MyHomePage(),
@@ -19,56 +19,94 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+//
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String text = "";
+
+  void changeText(String text) {
+    this.setState(() {
+      this.text = text;
+    });
+  } //callback function. simply changes the text above to basically what we have passed in
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Hello World!')), //menu bar
-      // body: Text('My name is paris okayh')); //just a text
-      body: TextInputWidget(),
-      // body: Row(
-      //   children: <Widget>[TestWidget(), TestWidget(), TestWidget()],
-    );
+        appBar:
+            AppBar(title: Text('Welcome to fariswheel world :.> ')), //menu bar
+        // body: Text('My name is paris okayh')); //just a text
+        body: Column(children: <Widget>[
+          TextInputWidget(this
+              .changeText), //callback function that we want to use when the button is pressed --
+          Text(this.text) // the first main text --
+        ])
+        // body: Row(
+        //   children: <Widget>[TestWidget(), TestWidget(), TestWidget()],
+        );
   }
-}
+} //the build method
 
+//
 class TextInputWidget extends StatefulWidget {
-  const TextInputWidget({super.key});
+  final Function(String) callback;
+
+  TextInputWidget(this.callback);
 
   @override
   State<TextInputWidget> createState() => _TextInputWidgetState();
 }
 
 class _TextInputWidgetState extends State<TextInputWidget> {
-  final controller = TextEditingController();
-  String text = ""; //3. text u entered
+  final controller =
+      TextEditingController(); // handles the Textfield so clearing and getting the text --
+  // String text = ""; //3. text u entered pt 3
 
   void dispose() {
     super.dispose();
     controller.dispose();
-  }
+  } //gets rid of resources when we're done with it --
 
-  void ChangeText(text) {
-    if (text == "Hello World!") {
-      controller.clear();
-      text = "";
-    } // this if statement basically cleares the text if the text placed here is the same as what the user has entered
-    setState(() {
-      this.text = text; //1. this forces the widget to refresh and rebuilt
-    });
+  // void ChangeText(text) {
+  //   if (text == "Hello World!") {
+  //     controller.clear();
+  //     text = "";
+  //   } // this if statement basically cleares the text if the text placed here is the same as what the user has inputed
+  //   setState(() {
+  //     this.text = text; //1. this forces the widget to refresh and rebuilt
+  //   });
+  // } pt 3
+
+  void click() {
+    widget.callback(controller.text);
+    controller.clear();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      TextField(
+    // return Column(children: <Widget>[ pt 3
+
+    return TextField(
         controller: this.controller,
         decoration: InputDecoration(
-            prefixIcon: Icon(Icons.message), labelText: "Type a message:"),
-        onChanged: (text) => this.ChangeText(text),
-      ),
-      Text(this.text) //2. redraws the widget with text from no3
-    ]);
+            prefixIcon: Icon(Icons.message),
+            labelText: "Type a message:",
+            suffixIcon: IconButton(
+              icon: Icon(Icons.send),
+              splashColor: Colors.blue,
+              tooltip: "Post message", //it pops up when u long press
+              onPressed: this.click,
+            )));
+    // onChanged: (text) => this.ChangeText(text), pt3
+
+    // Text(this.text) //2. redraws the widget with text from no3 pt 3
+    //] );
   }
 }
 
@@ -80,4 +118,4 @@ class _TextInputWidgetState extends State<TextInputWidget> {
 //       child: Text('Monkuu world'),
 //     );
 //   }
-// }
+// } pt 2
