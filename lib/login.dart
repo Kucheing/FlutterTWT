660556b4f11e40041219a77c4c457673e5fe6bc7
Faris Zulkiffli <22FTT1517@student.pb.edu.bn>
2024-07-20 14:28:02 +0800

    tweaking thedesign adding controller, decorations and name

--- lib/login.dart
+++ lib/login.dart
@@ -1,17 +1,51 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatefulWidget {
+class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Welcome to fariswheel world 🐼🎡;.> ')),
+        body: Body());
+  }
 }
 
-class _LoginPageState extends State<LoginPage> {
+class Body extends StatefulWidget {
+  const Body({super.key});
+
+  @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
   String name;
+  TextEditingController controller = new TextEditingController();
+
+  void click() {
+    this.name = controller.text;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Align(
+        alignment: Alignment.center,
+        child: Padding(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              controller: this.controller,
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.person),
+                labelText: "Type Your Name:",
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(width: 5, color: Colors.pink)),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.done),
+                  splashColor: Colors.green,
+                  tooltip: "Subit", //it pops up when u long press
+                  onPressed: this.click,
+                ),
+              ),
+            )));
   }
 }
